1badd8d28ae18a08f672d4a62eb800cf6580864e
han4solecode <farhan@solecode.id>
2024-07-24 13:01:03 +0700

    commit to fix everything

---
+++ C# and .net/oop/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    public class Person
+    {
+        public virtual void PrintName()
+        {
+            Console.WriteLine("I am a person!");
+        }
+    }
+
+    public class Trainer : Person
+    {
+        public override void PrintName()
+        {
+            Console.WriteLine("I am a trainer!");
+        }
+    }
+
+    public class Student : Person
+    {
+        public override void PrintName()
+        {
+            Console.WriteLine("I am a student!");
+        }
+    }
+}
