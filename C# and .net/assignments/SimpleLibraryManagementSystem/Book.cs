92d3a4c3e9aa9397747d8bdf09a552f056cfe44a
han4solecode <farhan@solecode.id>
2024-07-24 21:50:35 +0700

    fix: move Book class to another file

---
+++ C# and .net/assignments/SimpleLibraryManagementSystem/Book.cs
@@ -0,0 +1,20 @@
+namespace SimpleLibraryManagementSystem
+{
+    public class Book(string title, string author, int publicationYear)
+{
+    // getter props for reading field value
+    public string Title
+    {
+        get { return title; }
+    }
+    public string Author
+    {
+        get { return author; }
+    }
+    public int PublicationYear
+    {
+        get { return publicationYear; }
+    }
+
+}
+}
--- C# and .net/assignments/SimpleLibraryManagementSystem/Program.cs
+++ C# and .net/assignments/SimpleLibraryManagementSystem/Program.cs
@@ -36,24 +36,6 @@ class Program
     }
 }
 
-public class Book(string title, string author, int publicationYear)
-{
-    // getter props for reading field value
-    public string Title
-    {
-        get { return title; }
-    }
-    public string Author
-    {
-        get { return author; }
-    }
-    public int PublicationYear
-    {
-        get { return publicationYear; }
-    }
-
-}
-
 public class Library()
 {
     List<Book> bookList = [];
