535f5eaaf12bc70982abcaeacb66b1fbebd3ac1e
han4solecode <farhan@solecode.id>
2024-07-25 11:12:52 +0700

    feat: adding Dessert constructor with MenuItem constructor as base

--- C# and .net/mini-projects/OnlineFoodOrderingSystem/Dessert.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/Dessert.cs
@@ -5,6 +5,15 @@ namespace OnlineFoodOrderingSystem
         // Dessert prop
         public string? SugarLevel { get; set; }
 
+        // Dessert constructor
+        public Dessert(string sugarLevel, string name, int price, string description): base(name, price, description)
+        {
+            SugarLevel = sugarLevel;
+            this.name = name;
+            this.price = price;
+            this.description = description;
+        }
+
         // overriding CalculatePrice method from MenuItem abstract class
         public override int CalucatePrice()
         {
