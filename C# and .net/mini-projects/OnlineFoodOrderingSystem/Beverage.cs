982114bb5a8dbd1f14da298659e0f388fd994b6a
han4solecode <farhan@solecode.id>
2024-07-25 11:12:24 +0700

    feat: adding Beverage constructor with MenuItem constructor as base

--- C# and .net/mini-projects/OnlineFoodOrderingSystem/Beverage.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/Beverage.cs
@@ -5,6 +5,15 @@ namespace OnlineFoodOrderingSystem
         // Beverage prop
         public string? Size { get; set; }
 
+        // Beverage constructor
+        public Beverage(string size, string name, int price, string description): base(name, price, description)
+        {
+            this.Size = size;
+            this.name = name;
+            this.price = price;
+            this.description = description;
+        }
+
         // overriding CalculatePrice method from MenuItem abstract class
         public override int CalucatePrice()
         {
