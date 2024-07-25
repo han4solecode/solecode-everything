9650db6e3f403ecefdeb414c19f75cff65c61fcb
han4solecode <farhan@solecode.id>
2024-07-25 11:11:31 +0700

    feat: adding Food constructor with MenuItem constructor as base

--- C# and .net/mini-projects/OnlineFoodOrderingSystem/Food.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/Food.cs
@@ -5,6 +5,15 @@ namespace OnlineFoodOrderingSystem
         // Food prop
         public string? Spiciness { get; set; }
 
+        // Food constructor
+        public Food(string spiciness, string name, int price, string description): base(name, price, description)
+        {
+            Spiciness = spiciness;
+            this.name = name;
+            this.price = price;
+            this.description = description;
+        }
+
         // overriding CalculatePrice method from MenuItem abstract class
         public override int CalucatePrice()
         {
