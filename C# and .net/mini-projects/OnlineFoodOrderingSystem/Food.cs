cc8ef736ab6856c2b172daa93e2adc00f5b0e3aa
han4solecode <farhan@solecode.id>
2024-07-26 10:34:06 +0700

    style: add coments

--- C# and .net/mini-projects/OnlineFoodOrderingSystem/Food.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/Food.cs
@@ -1,11 +1,14 @@
 namespace OnlineFoodOrderingSystem
 {
+    // This class inherits MenuItem absract class
+    // We have to implement all abstract method from MenuItem through overriding it and return desired outcome
     public class Food : MenuItem
     {
         // Food prop
         public string? Spiciness { get; set; }
 
         // Food constructor
+        // Using base keyword so that all fields from MenuItem is inherited in the class constructor and we still be able to add prop unique to this class
         public Food(string spiciness, string name, int price, string description): base(name, price, description)
         {
             Spiciness = spiciness;
--- C# and .net/mini-projects/OnlineFoodOrderingSystem/IOrderingSystem.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/IOrderingSystem.cs
@@ -1,5 +1,8 @@
 namespace OnlineFoodOrderingSystem
 {
+    // Interface is used to make a "contract" to its concrete class
+    // At the concrete class, we have to implement all of the method of the interface
+    // Implemented method in each concrete class may be different, just keep in mind the method name and parameters
     public interface IOrderingSystem
     {
         // IOrderingSystem interface methods
