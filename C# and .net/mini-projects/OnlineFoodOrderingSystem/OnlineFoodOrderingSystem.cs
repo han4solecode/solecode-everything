1fbd7ff6f6e93f97f80eefa7e80de9b18d1a2452
han4solecode <farhan@solecode.id>
2024-07-26 22:19:06 +0700

    feat: implement CancelOrder and GetOrderStatus mehtod, add new props

--- C# and .net/mini-projects/OnlineFoodOrderingSystem/OnlineFoodOrderingSystem.cs
+++ C# and .net/mini-projects/OnlineFoodOrderingSystem/OnlineFoodOrderingSystem.cs
@@ -2,11 +2,22 @@ using System.Collections;
 
 namespace OnlineFoodOrderingSystem
 {
+    
     public class OnlineFoodOrderingSystem : IOrderingSystem
     {
         private List<Restaurant> restaurantList = [];
         private Dictionary<string, Order> placedOrderInRestaurant = [];
         private string? orderNumber;
+
+        // props
+        public List<Restaurant> RestaurantList
+        {
+            get => restaurantList;
+        }
+        public Dictionary<string, Order> PlacedOrderInRestaurant
+        {
+            get => placedOrderInRestaurant;
+        }
 
         // IOrderingSystem methods implementation
         // method for adding Restaurant to the system
@@ -25,8 +36,11 @@ namespace OnlineFoodOrderingSystem
             // check if restaurant exist
             if (restoToPlaceOrder != null)
             {
+                // create order number with Guid 
+                orderNumber = Guid.NewGuid().ToString();
+
                 // create new Order object to be inserted to Restaurant
-                Order newOrder = new(orderedItems);
+                Order newOrder = new(orderedItems, "placed");
 
                 // calculate total order cost
                 newOrder.CalculateTotal();
@@ -34,9 +48,6 @@ namespace OnlineFoodOrderingSystem
                 // insert new order with ReceiveOrder method
                 restoToPlaceOrder.ReceiveOrders(newOrder);
                 Console.WriteLine("Your order has been placed at {0}", restoToPlaceOrder.Name);
-
-                // create order number with Guid 
-                orderNumber = Guid.NewGuid().ToString();
 
                 // insert order number and its order to a dictionary
                 placedOrderInRestaurant.Add(orderNumber, newOrder);
@@ -58,18 +69,19 @@ namespace OnlineFoodOrderingSystem
             // check if order number exist
             if (placedOrderInRestaurant.TryGetValue(orderNumber, out Order? order))
             {
-                Order themOrderInResto = order;
-
                 Console.WriteLine("\n==== Ordered Items ====");
+                Console.WriteLine("Order Number: {0}", orderNumber);
+                Console.WriteLine("Order Status: {0}", order.Status);
+                Console.WriteLine("=======================");
 
-                foreach (MenuItem item in themOrderInResto.MenuItemList)
+                foreach (MenuItem item in order.MenuItemList)
                 {
                     // use MenuInfo() method
                     Console.WriteLine(item.MenuInfo());
                     Console.WriteLine("-----------------------");
                 }
 
-                Console.WriteLine("Total cost: {0}\n", themOrderInResto.TotalPrice);
+                Console.WriteLine("Total cost: {0}\n", order.TotalPrice);
             }
             else
             {
@@ -77,8 +89,32 @@ namespace OnlineFoodOrderingSystem
             }
         }
 
-        public void CancelOrder() { }
-        public void GetOrderStatus() { }
+        // method for canceling placed order
+        public void CancelOrder(string orderNumber)
+        {
+            if (placedOrderInRestaurant.TryGetValue(orderNumber, out Order? order))
+            {
+                order.Status = "canceled";
+                Console.WriteLine("Order with {0} order number is canceled", orderNumber);
+            }
+            else
+            {
+                Console.WriteLine("Order with {0} order number is does not exist", orderNumber);
+            }
+        }
+
+
+        public void GetOrderStatus(string orderNumber)
+        {
+            if (placedOrderInRestaurant.TryGetValue(orderNumber, out Order? order))
+            {
+                Console.WriteLine("{0} order status: {1}", orderNumber, order.Status);
+            }
+            else
+            {
+                Console.WriteLine("Order with {0} order number is does not exist", orderNumber);
+            }
+        }
 
     }
 }
