acd56a8076a4e72cfd3cccd94194d484d8530041
han4solecode <farhan@solecode.id>
2024-07-29 18:41:40 +0700

    use proper return value using action result

--- ASP.NET Core Web Api/webapi-controller/Controllers/ShirtsController.cs
+++ ASP.NET Core Web Api/webapi-controller/Controllers/ShirtsController.cs
@@ -7,6 +7,14 @@ namespace webapi_controller.Controllers
     [Route("[controller]")]
     public class ShirtsController : ControllerBase
     {
+        private readonly List<Shirt> shirtList =
+        [
+            new Shirt {ID = 1, Brand = "Brand 1", Color = "Black", Gender = "male", Price = 20, Size = 8},
+            new Shirt {ID = 2, Brand = "Brand 2", Color = "Yellow", Gender = "male", Price = 20, Size = 10},
+            new Shirt {ID = 3, Brand = "Brand 3", Color = "Pink", Gender = "female", Price = 20, Size = 6},
+            new Shirt {ID = 4, Brand = "Brand 4", Color = "Purle", Gender = "female", Price = 20, Size = 7}
+        ];
+
         [HttpGet]
         public string Get()
         {
@@ -14,13 +22,24 @@ namespace webapi_controller.Controllers
         }
 
         [HttpGet("{id}")]
-        public string GetByID(int id)
+        public IActionResult GetByID(int id)
         {
-            return $"Get shirt with ID: {id}";
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var shirt = shirtList.FirstOrDefault(s => s.ID == id);
+            if (shirt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shirt);
         }
 
         [HttpPost]
-        public string Post([FromBody]Shirt shirt)
+        public string Post([FromBody] Shirt shirt)
         {
             return $"New {shirt.Brand} shirt is created with {shirt.Color} color, size {shirt.Size}, for {shirt.Gender}, and a ${shirt.Price} price tag";
         }
