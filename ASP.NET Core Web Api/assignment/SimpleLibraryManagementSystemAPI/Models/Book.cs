0f66cdf678275acbb27c660f23604c9c91c8efbd
han4solecode <farhan@solecode.id>
2024-07-29 22:16:03 +0700

    feat: create book model

---
+++ ASP.NET Core Web Api/assignment/SimpleLibraryManagementSystemAPI/Models/Book.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleLibraryManagementSystemAPI.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$", ErrorMessage ="ISBN is invalid")]
+        public string? ISBN { get; set; }
+    }
+}
