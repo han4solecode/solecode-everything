c18286df17eea78f856f110bc4415ba099ce03fb
han4solecode <farhan@solecode.id>
2024-07-29 11:20:20 +0700

    style: format page

--- ASP.NET Core Web Api/webapi-controller/Controllers/ShirtsController.cs
+++ ASP.NET Core Web Api/webapi-controller/Controllers/ShirtsController.cs
@@ -1,38 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace webapi_controller.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class ShirtsController : ControllerBase
+namespace webapi_controller.Controllers
 {
-    [HttpGet]
-    public string Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class ShirtsController : ControllerBase
     {
-        return "Get all shirts";
-    }
+        [HttpGet]
+        public string Get()
+        {
+            return "Get all shirts";
+        }
 
-    [HttpGet("{id}")]
-    public string GetByID(int id)
-    {
-        return $"Get shirt with ID: {id}";
-    }
+        [HttpGet("{id}")]
+        public string GetByID(int id)
+        {
+            return $"Get shirt with ID: {id}";
+        }
 
-    [HttpPost]
-    public string Post()
-    {
-        return "Post a new shirt ";
-    }
+        [HttpPost]
+        public string Post()
+        {
+            return "Post a new shirt ";
+        }
 
-    [HttpPut("{id}")]
-    public string Put(int id)
-    {
-        return $"Update shirt with ID: {id}";
-    }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Update shirt with ID: {id}";
+        }
 
-    [HttpDelete("{id}")]
-    public string Delete(int id)
-    {
-        return $"Delete shirt with ID: {id}";
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Delete shirt with ID: {id}";
+        }
     }
 }
