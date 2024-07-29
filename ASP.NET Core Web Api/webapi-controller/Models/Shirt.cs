e51c95fe3e87fe4d116beaffb7660fd8141fb5f2
han4solecode <farhan@solecode.id>
2024-07-29 11:23:14 +0700

    create shirt model

---
+++ ASP.NET Core Web Api/webapi-controller/Models/Shirt.cs
@@ -0,0 +1,12 @@
+namespace webapi_controller.Models
+{
+    public class Shirt
+    {
+        public int ID { get; set; }
+        public string? Brand { get; set; }
+        public string? Color { get; set; }
+        public int Size { get; set; }
+        public string? Gender { get; set; }
+        public double Price { get; set; }
+    }
+}
