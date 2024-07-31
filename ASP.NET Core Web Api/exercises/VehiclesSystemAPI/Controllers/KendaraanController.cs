c47e57aea0b586f8dc252317a300a1c07a9660f6
han4solecode <farhan@solecode.id>
2024-07-31 11:39:29 +0700

    feat: add openAPI documentation

--- ASP.NET Core Web Api/exercises/VehiclesSystemAPI/Controllers/KendaraanController.cs
+++ ASP.NET Core Web Api/exercises/VehiclesSystemAPI/Controllers/KendaraanController.cs
@@ -17,12 +17,22 @@ namespace VehiclesSystemAPI.Controllers
             _kendaraanService = kendaraanService;
         }
 
+        /// <summary>
+        /// Get all kendaraan data
+        /// </summary>
+        /// 
+        /// <returns>This endpoint returns a list of kendaraan.</returns>
         [HttpGet]
         public IActionResult GetAllKendaraan()
         {
             return Ok(_kendaraanService.GetAllKendaraan());
         }
 
+        /// <summary>
+        /// Get kendaraan by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>This endpoint returns a kendaraan.</returns>
         [HttpGet("{id}")]
         public IActionResult GetKendaranById(int id)
         {
@@ -41,6 +51,11 @@ namespace VehiclesSystemAPI.Controllers
             return Ok(kendaraan);
         }
 
+        /// <summary>
+        /// Create a new kendaraan mobil
+        /// </summary>
+        /// <param name="mobil"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("mobil")]
         public IActionResult AddMobil([FromBody] Mobil mobil)
@@ -49,6 +64,11 @@ namespace VehiclesSystemAPI.Controllers
             return Created($"kendaraan/{mobil.Id}", mobil);
         }
 
+        /// <summary>
+        /// Create a new kendaraan motor
+        /// </summary>
+        /// <param name="motor"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("motor")]
         public IActionResult AddMotor([FromBody] Motor motor)
@@ -57,6 +77,11 @@ namespace VehiclesSystemAPI.Controllers
             return Created($"kendaraan/{motor.Id}", motor);
         }
 
+        /// <summary>
+        /// Create a new kendaraan mobil listrik
+        /// </summary>
+        /// <param name="mobilListrik"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("mobillistrik")]
         public IActionResult AddMobilListrik([FromBody] MobilListrik mobilListrik)
@@ -65,6 +90,11 @@ namespace VehiclesSystemAPI.Controllers
             return Created($"kendaraan/{mobilListrik.Id}", mobilListrik);
         }
 
+        /// <summary>
+        /// Create a new kendaraan motor listrik
+        /// </summary>
+        /// <param name="motorListrik"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("motorlistrik")]
         public IActionResult AddMotorListrik([FromBody] MotorListrik motorListrik)
@@ -73,6 +103,12 @@ namespace VehiclesSystemAPI.Controllers
             return Created($"kendaraan/{motorListrik.Id}", motorListrik);
         }
 
+        /// <summary>
+        /// Update a kendaraan by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="inputKendaraan"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public IActionResult UpdateKendaraan(int id, [FromBody] Kendaraan inputKendaraan)
         {
@@ -91,6 +127,11 @@ namespace VehiclesSystemAPI.Controllers
             return Ok(kendaraan);
         }
 
+        /// <summary>
+        /// Delete a kendaraan by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public IActionResult DeleteKendaran(int id)
         {
@@ -109,6 +150,11 @@ namespace VehiclesSystemAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Charge all kendaraan listrik
+        /// </summary>
+        /// <param name="jumlah"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("charge")]
         public IActionResult ChargeKendaraanListrik([FromQuery] int jumlah)
@@ -117,6 +163,11 @@ namespace VehiclesSystemAPI.Controllers
             return NoContent();
         }
 
+
+        /// <summary>
+        /// Display all kendaraan info
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("info")]
         public IActionResult TamplikanSemuaKendaraan()
