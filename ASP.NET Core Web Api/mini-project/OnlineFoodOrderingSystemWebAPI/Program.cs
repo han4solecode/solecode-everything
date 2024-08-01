09bddeef89f73e5c90c5e80effd27d0531dbc5c6
han4solecode <farhan@solecode.id>
2024-08-01 23:07:29 +0700

    feat: add swagger configs

--- ASP.NET Core Web Api/mini-project/OnlineFoodOrderingSystemWebAPI/Program.cs
+++ ASP.NET Core Web Api/mini-project/OnlineFoodOrderingSystemWebAPI/Program.cs
@@ -1,3 +1,7 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using OnlineFoodOrderingSystemWebAPI.Interfaces;
+using OnlineFoodOrderingSystemWebAPI.Services;
 
 namespace OnlineFoodOrderingSystemWebAPI;
 
@@ -12,15 +16,43 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+
+        var info = new OpenApiInfo()
+        {
+            Title = "Online Food Ordering System API Documentation",
+            Version = "v1",
+            Description = "A food ordering system web API",
+            Contact = new OpenApiContact()
+            {
+                Name = "M. Farhan Athaullah",
+                Email = "farhan@solecode.id",
+            }
+        };
+
+        builder.Services.AddSwaggerGen(opt => {
+            opt.SwaggerDoc("v1", info);
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            opt.IncludeXmlComments(xmlPath);
+        });
+
+        builder.Services.AddSingleton<IMenuService, MenuService>();
+        builder.Services.AddSingleton<ICustomerService, CustomerService>();
+        builder.Services.AddSingleton<IOrderService, OrderService>();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwagger(u => {
+                u.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(c => {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Online Food Ordering System API v1");
+            });
         }
 
         app.UseHttpsRedirection();
