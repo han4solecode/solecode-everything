9e50dbb8f2280bcbcce496f4f28cf315c36feba5
han4solecode <farhan@solecode.id>
2024-08-01 10:43:15 +0700

    feat: create ICustomerService for CustomerService

---
+++ ASP.NET Core Web Api/mini-project/OnlineFoodOrderingSystemWebAPI/Interfaces/ICustomerService.cs
@@ -0,0 +1,17 @@
+using OnlineFoodOrderingSystemWebAPI.Models;
+
+namespace OnlineFoodOrderingSystemWebAPI.Interfaces
+{
+    public interface ICustomerService
+    {
+        Customer AddCustomer(Customer customer);
+
+        List<Customer> GetAllCustomer();
+
+        Customer GetCustomerById(int id);
+
+        Customer UpdateCustomer(int id, Customer inputCustomer);
+
+        bool DeleteCustomer(int id);
+    }
+}
