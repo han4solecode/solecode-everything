d77c36062ca21aa20ec747f16e558811ce46e45b
han4solecode <farhan@solecode.id>
2024-08-05 19:00:53 +0700

    rename project name

---
+++ Entity Framework Core/assignment/SimpleLibraryManagementSystemAPI-Postgres/Models/Book.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleLibrartyManagementSystemAPI_Postgres.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$", ErrorMessage = "ISBN is invalid")]
+        public string? ISBN { get; set; }
+    }
+}
