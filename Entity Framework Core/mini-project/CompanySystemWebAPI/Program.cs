98f77deec6d15ec53ad8c5ab1cb1a3018a2cd221
han4solecode <farhan@solecode.id>
2024-08-08 12:40:26 +0700

    feat: inject api versioning middleware to program main

--- Entity Framework Core/mini-project/CompanySystemWebAPI/Program.cs
+++ Entity Framework Core/mini-project/CompanySystemWebAPI/Program.cs
@@ -1,4 +1,5 @@
 
+using Asp.Versioning;
 using CompanySystemWebAPI.Data;
 using CompanySystemWebAPI.Interfaces;
 using CompanySystemWebAPI.Services;
@@ -19,6 +20,21 @@ public class Program
         });
 
         builder.Services.AddControllers();
+
+        builder.Services.AddApiVersioning(option => {
+            option.AssumeDefaultVersionWhenUnspecified = true;
+            option.DefaultApiVersion = new ApiVersion(1, 0);
+            option.ReportApiVersions = true;
+            option.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version"),
+                new HeaderApiVersionReader("X-Version"),
+                new MediaTypeApiVersionReader("ver")
+            );
+        }).AddApiExplorer(option => {
+            option.GroupNameFormat = "'v'VVV";
+            option.SubstituteApiVersionInUrl = true;
+        });
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
