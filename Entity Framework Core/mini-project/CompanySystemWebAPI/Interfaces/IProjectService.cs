5d3f3d4b53c083a7fe93ed402c641fc650b4b8ce
han4solecode <farhan@solecode.id>
2024-08-08 10:10:02 +0700

    feat: create project service interface

---
+++ Entity Framework Core/mini-project/CompanySystemWebAPI/Interfaces/IProjectService.cs
@@ -0,0 +1,17 @@
+using CompanySystemWebAPI.Models;
+
+namespace CompanySystemWebAPI.Interfaces
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<Project>> GetAllProject(int recordsPerPage, int currentPage);
+
+        Task<Project> GetProjectById(int id);
+
+        Task<Project> AddProject(Project project);
+
+        Task<Project> UpdateProject(int id, Project inputProject);
+
+        Task<bool> DeleteProject();
+    }
+}
