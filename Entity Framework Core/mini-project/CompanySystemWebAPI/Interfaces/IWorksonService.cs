83ef8e57d7cc529dc4b8771fc2edf1d953ccfe60
han4solecode <farhan@solecode.id>
2024-08-08 10:14:08 +0700

    feat: create workson service interface

---
+++ Entity Framework Core/mini-project/CompanySystemWebAPI/Interfaces/IWorksonService.cs
@@ -0,0 +1,17 @@
+using CompanySystemWebAPI.Models;
+
+namespace CompanySystemWebAPI.Interfaces
+{
+    public interface IWorksonService
+    {
+        Task<IEnumerable<Workson>> GetAllWorkson(int recordsPerPage, int currentPage);
+
+        Task<Workson> GetWorksonById(int id);
+
+        Task<Workson> AddWorkson(Workson workson);
+
+        Task<Workson> UpdateWorkson(int id, Workson inputWorkson);
+
+        Task<bool> DeleteWorkson(int id);
+    }
+}
