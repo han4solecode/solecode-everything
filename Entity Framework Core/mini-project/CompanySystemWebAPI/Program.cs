37eacfa7012b6f99305a0d80c01ce62bb15aa253
han4solecode <farhan@solecode.id>
2024-08-08 09:48:32 +0700

    feat: inject dbcontext to program main

--- Entity Framework Core/mini-project/CompanySystemWebAPI/Program.cs
+++ Entity Framework Core/mini-project/CompanySystemWebAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using CompanySystemWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace CompanySystemWebAPI;
 
 public class Program
@@ -8,6 +11,10 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("PostgresDbConnection");
+        builder.Services.AddDbContext<AppDbContext>(option => {
+            option.UseNpgsql(connectionString);
+        });
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
