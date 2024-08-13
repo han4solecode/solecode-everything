726cc1cd695644441a7982f6fa70718058c5a27e
han4solecode <farhan@solecode.id>
2024-08-13 10:45:25 +0700

    fix: rescaffold dbcontext

--- Application Conf and Dependencies/assignment/SimpleLibraryManagementSystemWebAPI/Models/Book.cs
+++ Application Conf and Dependencies/assignment/SimpleLibraryManagementSystemWebAPI/Models/Book.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace SimpleLibraryManagementSystemWebAPI.Models;
 
+[Table("books")]
 public partial class Book
 {
+    [Key]
+    [Column("bookid")]
     public int Bookid { get; set; }
 
+    [Column("title")]
+    [StringLength(255)]
     public string Title { get; set; } = null!;
 
+    [Column("author")]
+    [StringLength(255)]
     public string Author { get; set; } = null!;
 
+    [Column("publicationyear")]
     public DateOnly Publicationyear { get; set; }
 
+    [Column("isbn")]
+    [StringLength(17)]
     public string Isbn { get; set; } = null!;
+
+    [InverseProperty("Book")]
+    public virtual ICollection<Lending> Lendings { get; set; } = new List<Lending>();
 }
---
+++ Application Conf and Dependencies/assignment/SimpleLibraryManagementSystemWebAPI/Models/Lending.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleLibraryManagementSystemWebAPI.Models;
+
+[Table("lendings")]
+public partial class Lending
+{
+    [Key]
+    [Column("lendingid")]
+    public int Lendingid { get; set; }
+
+    [Column("userid")]
+    public int Userid { get; set; }
+
+    [Column("bookid")]
+    public int Bookid { get; set; }
+
+    [Column("borrowdate")]
+    public DateOnly? Borrowdate { get; set; }
+
+    [Column("returndate")]
+    public DateOnly? Returndate { get; set; }
+
+    [ForeignKey("Bookid")]
+    [InverseProperty("Lendings")]
+    public virtual Book Book { get; set; } = null!;
+
+    [ForeignKey("Userid")]
+    [InverseProperty("Lendings")]
+    public virtual User User { get; set; } = null!;
+}
