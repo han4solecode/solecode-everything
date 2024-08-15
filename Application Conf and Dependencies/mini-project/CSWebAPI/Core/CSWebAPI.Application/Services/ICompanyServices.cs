dbf9d1c34991c09040fc12c2f68cf4dc35083772
han4solecode <farhan@solecode.id>
2024-08-15 12:55:11 +0700

    feat: update services

--- Application Conf and Dependencies/mini-project/CSWebAPI/Core/CSWebAPI.Application/Services/ICompanyServices.cs
+++ Application Conf and Dependencies/mini-project/CSWebAPI/Core/CSWebAPI.Application/Services/ICompanyServices.cs
@@ -16,6 +16,12 @@ namespace CSWebAPI.Application.Services
         // d
         Task<IEnumerable<Employee>> FemaleManager();
 
+        // e
+        Task<IEnumerable<Employee>> EmpNotManager();
+
+        // f
+        Task<IEnumerable<Project>> PlanningProjects();
+
         // g
         Task<IEnumerable<Project>> NoEmpProject();
 
@@ -28,11 +34,32 @@ namespace CSWebAPI.Application.Services
         // j
         Task<IEnumerable<Employee>> DueRetireManager();
 
+        // k
+        Task<IEnumerable<Object>> FemEmpHoursWorked();
+
         // l
         Task<int> FemaleManagerCount();
 
         // m
         Task<IEnumerable<Project>> ITAndHRProjects();
+
+        // n
+        Task<IEnumerable<Object>> EmpNotManagerAndSupervisor();
+
+        // o
+        Task<IEnumerable<Employee>> FemaleManagerProjects();
+
+        // p
+        Task<IEnumerable<Object>> MaxAndMinWorkHours();
+
+        // q
+        Task<IEnumerable<Object>> EmpTotalWorkHours();
+
+        // r
+        Task<IEnumerable<Object>> EmpAge();
+
+        // s
+        Task<IEnumerable<Object>> EmpProjInfo();
 
         // t
         Task<IEnumerable<Object>> ManagerUnder40();
