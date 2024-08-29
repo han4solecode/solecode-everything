8bf115946d839a250ddf2fcc4172e1b55c21aa11
han4solecode <farhan@solecode.id>
2024-08-29 11:54:30 +0700

    feat: create repository interfaces

---
+++ Authentication & Authorization GPP/mini-project/HRIS/Core/HRIS.Application/Persistance/IProjectRepository.cs
@@ -0,0 +1,10 @@
+using HRIS.Application.Persistance.Common;
+using HRIS.Domain.Entity;
+
+namespace HRIS.Application.Persistance
+{
+    public interface IProjectRepository : IBaseRepository<Project>
+    {
+        
+    }
+}
