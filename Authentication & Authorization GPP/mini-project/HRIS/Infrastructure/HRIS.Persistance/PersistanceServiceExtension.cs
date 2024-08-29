b67c23bf6e140556d160618881990ff3c8bef08e
han4solecode <farhan@solecode.id>
2024-08-29 14:56:10 +0700

    feat: register repositories to service

--- Authentication & Authorization GPP/mini-project/HRIS/Infrastructure/HRIS.Persistance/PersistanceServiceExtension.cs
+++ Authentication & Authorization GPP/mini-project/HRIS/Infrastructure/HRIS.Persistance/PersistanceServiceExtension.cs
@@ -1,5 +1,8 @@
+using HRIS.Application.Persistance;
 using HRIS.Domain.Entity;
+using HRIS.Persistance.Repository;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,7 +12,16 @@ namespace HRIS.Persistance
     {
         public static void ConfigurePersistance(this IServiceCollection services, IConfiguration configuration)
         {
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(opt => {
+                opt.UseNpgsql(connection);
+            });
 
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<IWorksonRepository, WorksonRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IEmpDependentRepository, EmpDependentRepository>();
         }
 
         public static void ConfigureIdentity(this IServiceCollection services)
