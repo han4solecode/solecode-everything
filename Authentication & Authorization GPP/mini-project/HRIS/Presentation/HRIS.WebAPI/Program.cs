79b86ac5cdabf1484fd2b4648dfe661e34c0e766
han4solecode <farhan@solecode.id>
2024-08-29 10:21:20 +0700

    feat: add services and app settings

--- Authentication & Authorization GPP/mini-project/HRIS/Presentation/HRIS.WebAPI/Program.cs
+++ Authentication & Authorization GPP/mini-project/HRIS/Presentation/HRIS.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using HRIS.Persistance;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HRIS.WebAPI;
 
@@ -8,6 +12,29 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.ConfigurePersistance(builder.Configuration);
+        builder.Services.ConfigureIdentity();
+
+        var configuration = builder.Configuration;
+
+        builder.Services.AddAuthentication(opt => {
+            opt.DefaultAuthenticateScheme =
+            opt.DefaultChallengeScheme =
+            opt.DefaultForbidScheme =
+            opt.DefaultScheme =
+            opt.DefaultSignInScheme =
+            opt.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(opt => {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidateAudience = true,
+                ValidAudience= configuration["JWT:Audience"],
+                ValidateIssuerSigningKey= true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SigningKey"]!)),
+            };
+        });
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +52,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
