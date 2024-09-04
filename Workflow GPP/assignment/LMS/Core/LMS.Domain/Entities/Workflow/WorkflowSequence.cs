d79784dfa2ca08bf415d7629dce06b0565727532
han4solecode <farhan@solecode.id>
2024-09-04 10:58:34 +0700

    feat: remove NextStepId, add CurrentStepId and NextStepId navigation

--- Workflow GPP/assignment/LMS/Core/LMS.Domain/Entities/Workflow/WorkflowSequence.cs
+++ Workflow GPP/assignment/LMS/Core/LMS.Domain/Entities/Workflow/WorkflowSequence.cs
@@ -22,14 +22,22 @@ namespace LMS.Domain.Entities.Workflow
         public AppRole RequiredRoleIdNavigation { get; set; } = null!;
 
         // reference to WorkflowSequence as a NextStepId
-        public int? NextStepId { get; set; }
-        [ForeignKey("NextStepId")]
-        public WorkflowSequence? NextStepIdNavigation { get; set; }
+        // public int? NextStepId { get; set; }
+        // [ForeignKey("NextStepId")]
+        // public WorkflowSequence? NextStepIdNavigation { get; set; }
 
         // navigation to Request
         public ICollection<Request> Requests { get; set; } = [];
 
         // navigation to WorkflowAction
         public ICollection<WorkflowAction> WorkflowActions { get; set; } = [];
+
+        // navigation to NextStepRule CurrentStepId
+        [InverseProperty("CurrentStepIdNavigation")]
+        public ICollection<NextStepRule> CurrentStepIds { get; set; } = [];
+
+        // navigation to NextStepRule NextStepId
+        [InverseProperty("NextStepIdNavigation")]
+        public ICollection<NextStepRule> NextStepIds { get; set; } = [];
     }
 }
