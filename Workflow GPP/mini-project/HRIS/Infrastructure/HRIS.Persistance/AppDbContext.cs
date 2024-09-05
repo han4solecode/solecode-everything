f4079a66c124d81a9f2d2021d05508a3fa100b83
han4solecode <farhan@solecode.id>
2024-09-05 10:26:16 +0700

    feat: add Workflow DbSets and update IdentityDbContext

--- Workflow GPP/mini-project/HRIS/Infrastructure/HRIS.Persistance/AppDbContext.cs
+++ Workflow GPP/mini-project/HRIS/Infrastructure/HRIS.Persistance/AppDbContext.cs
@@ -1,10 +1,11 @@
 using HRIS.Domain.Entity;
+using HRIS.Domain.Entity.Workflow;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HRIS.Persistance
 {
-    public class AppDbContext : IdentityDbContext<Employee>
+    public class AppDbContext : IdentityDbContext<Employee, AppRole, string>
     {
         public AppDbContext() { }
         
@@ -18,26 +19,18 @@ namespace HRIS.Persistance
             optionsBuilder.UseNpgsql("Host=localhost; Database=HRIS2; username=solecode; Password=12344321");
         }
 
-        // protected override void OnModelCreating(ModelBuilder modelBuilder)
-        // {
-        //     modelBuilder.Entity<Employee>(entity =>
-        //     {
-        //         entity.HasKey(e => e.Id).HasName("employees_pkey");
-
-        //         // entity.Property(e => e.Status).HasDefaultValueSql("'Active'::character varying");
-        //         // entity.Property(e => e.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-        //         entity.HasOne(d => d.Department).WithMany(p => p.Employees)
-        //             .OnDelete(DeleteBehavior.SetNull)
-        //             .HasConstraintName("fk_AspNetUsers_departments_deptno");
-        //     });
-        // }
-
-        // public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Workson> Worksons { get; set; }
         public DbSet<EmpDependent> EmpDependents { get; set; }
         public DbSet<Location> Locations { get; set; }
+
+        // Workflow DbSets
+        public DbSet<Workflow> Workflows { get; set; }
+        public DbSet<WorkflowSequence> WorkflowSequences { get; set; }
+        public DbSet<NextStepRule> NextStepRules { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<Process> Processes { get; set; }
+        public DbSet<WorkflowAction> WorkflowActions { get; set; }
     }
 }
