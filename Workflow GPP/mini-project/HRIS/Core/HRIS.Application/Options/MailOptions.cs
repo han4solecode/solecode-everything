5eb35774a8d7d13dbf332d8b46b435f60e99bf0d
han4solecode <farhan@solecode.id>
2024-09-05 09:08:48 +0700

    feat: create mail options for grabbing MailSettings

---
+++ Workflow GPP/mini-project/HRIS/Core/HRIS.Application/Options/MailOptions.cs
@@ -0,0 +1,15 @@
+namespace HRIS.Application.Options
+{
+    public class MailOptions
+    {
+        public const string MailSettings = "MailSettings";
+
+        public string EmailId { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public string UserName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public string Host { get; set; } = String.Empty;
+        public int Port { get; set; }
+        public bool UseSSL { get; set; }
+    }
+}
